e7bfc297893885f8fb58e0937bf6b15b7b7af163
gwbyun <bgw6051@naver.com>
2022-08-03 19:42:31 +0900

    exit_condition_difficult

--- graph_bfs/baek_7576_bfs.py
+++ graph_bfs/baek_7576_bfs.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug  1 21:44:29 2022
+Created on Wed Aug  3 18:52:37 2022
 
 @author: gw
 """
-
+import sys
 from collections import deque
 
 m, n = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(n)]
+matrix = [list(map(int,input().split())) for _ in range(n)]
 queue = deque([])
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 res = 0
+
+#m = x n =y
 
 for i in range(n):
     for j in range(m):
-        if matrix[i][j] == 1:
-            queue.append([i, j])
-
+        if matrix[i][j] ==1:
+            queue.append([i,j])
+            
 def bfs():
     while queue:
-        x, y = queue.popleft()
+        x,y = queue.popleft()
         for i in range(4):
-            nx, ny = dx[i] + x, dy[i] + y
-            if 0 <= nx < n and 0 <= ny < m and matrix[nx][ny] == 0:
-                matrix[nx][ny] = matrix[x][y] + 1
-                queue.append([nx, ny])
-
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if 0<= nx <n:
+                
+                
 bfs()
+
 for i in matrix:
     for j in i:
-        if j == 0:
+        if j ==0:
             print(-1)
-            exit(0)
-    res = max(res, max(i))
-print(res - 1
+            
+            
